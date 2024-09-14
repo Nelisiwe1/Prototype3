25dc84fa9f23bf0243a897326edb9eb9f2570d12
Nelisiwe Ngqeme <nelisiwengqeme3@gmail.com>
2024-09-14 16:58:48 +0200

    fist commit

--- Assets/Challenge 3/Scripts/MoveLeftX.cs
+++ Assets/Challenge 3/Scripts/MoveLeftX.cs
@@ -18,9 +18,9 @@ public class MoveLeftX : MonoBehaviour
     void Update()
     {
         // If game is not over, move to the left
-        if (playerControllerScript.gameOver)
-        {
-            transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
+        
+        if(playerControllerScript.gameOver == false){
+             transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
         }
 
         // If object goes off screen that is NOT the background, destroy it
